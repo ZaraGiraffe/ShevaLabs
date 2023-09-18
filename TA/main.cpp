852b706bc59e5e21cee8c477750e4c6d10fdb561
Zahar <znaumets@gmail.com>
2023-09-18 12:51:11 +0300

    I made pretty main file

--- TA/main.cpp
+++ TA/main.cpp
@@ -10,19 +10,18 @@ using namespace std;
 
 int main() {
 	vector<Rule> rules;
-    string infilename = "./in_local";
+    string infilename = "./in_local_dz";
     auto check = parse_file(infilename, rules);
     cout << check.message << '\n';
 
     if (!check.error) {
-        string outfile1 = "./withoutE";
-        string outfile2 = "./out_local";
+        string outfile1 = "./middle_local";
+        string outfile2 = "./final_local";
 
         delete_null_rules(rules);
         print_all_rules(outfile1, rules);
 
         chomsky_normal_form(rules);
         print_all_rules(outfile2, rules);
-        
     }
 }
