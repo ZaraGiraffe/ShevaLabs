729bfbf811074afaa566f4fad2eb904f5c3a6210
Zahar <znaumets@gmail.com>
2023-10-02 10:47:42 +0300

    U've created is_bracket and is_space and started to parse file

--- SP/lab3/main.cpp
+++ SP/lab3/main.cpp
@@ -1,4 +1,7 @@
 #include<string> 
+#include<fstream>
+#include<vector>
+#include<set>
 
 using namespace std;
 
@@ -15,11 +18,70 @@ struct Lexem {
     }
 
     string to_str() {
-        return "<" + name + " ," + type + ">";
+        return "<" + name + ", " + type + ">";
     }
 };
 
 
-int main() {
 
+set<string> brackets;
+
+void init_brackets() {
+    ifstream bracket_file("./brackets");
+    while(bracket_file.peek() != EOF) {
+        char br = bracket_file.get();
+        brackets.insert(string(1, br));
+    }
+    bracket_file.close();
+}
+
+int is_bracket(const string& now) {
+    return brackets.find(now) != brackets.end();
+}
+
+
+
+set<string> spaces;
+
+void init_spaces() {
+    string sp = " \t\n";
+    for (char ch : sp) 
+        spaces.insert(string(1, ch));
+}
+
+int is_space(const string& now) {
+    return spaces.find(now) != spaces.end();
+}
+
+
+
+vector<Lexem> build_lexems(const char* filename) {
+    ifstream file(filename);
+    vector<Lexem> lexems;
+
+    string now = "";
+    while (file.peek() != EOF) {
+        now.push_back(file.get());
+
+        if (is_space(now)) {
+            now.clear();
+        }
+
+        else if (is_bracket(now)) {
+
+        }
+    }
+
+    file.close();
+    return lexems;
+}
+
+
+void init_symbols() {
+    
+}
+
+
+int main() {
+    
 }
